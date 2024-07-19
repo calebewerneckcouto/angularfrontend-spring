ba816cf89a51f78c5742368b399e41ced994b0ba
calebewerneckcouto <calebewerneck@hotmail.com>
2024-07-19 00:37:42 -0300

    atualizando

--- src/app/server.js
+++ src/app/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/CURSO-ANGULAR-REST'));
+app.use(express.static(__dirname + '/dist/Curso-Angular-REST'));
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/CURSO-ANGULAR-REST/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Curso-Angular-REST/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
