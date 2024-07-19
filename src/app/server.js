e06f6e5cc09b5fecb4e3eb73c558eb692a35f261
calebewerneckcouto <calebewerneck@hotmail.com>
2024-07-19 01:02:32 -0300

    atualizando o projeto

--- src/app/server.js
+++ src/app/server.js
@@ -3,15 +3,16 @@ const path = require('path');
 
 const app = express();
 
-// Configuração para servir arquivos estáticos
+// Definindo o diretório onde os arquivos estáticos estão localizados
 app.use(express.static(path.join(__dirname, 'dist', 'Curso-Angular-REST')));
 
-// Rota para tratar todas as requisições GET
+// Configurando o roteamento para servir o index.html para todas as rotas
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'Curso-Angular-REST', 'index.html'));
 });
 
-// Inicia o servidor na porta especificada pelo ambiente ou na porta 8080
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT || 8080}`);
+// Iniciando o servidor na porta definida pelo ambiente ou na porta 8080
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
